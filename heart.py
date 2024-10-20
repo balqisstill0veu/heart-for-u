58e0a770acb6a8a1f63ead6359bf9fe0463581ca
balqisstill0veu <wolfiegirlss00@gmail.com>
2024-10-20 23:44:50 +0700

    heart.py
    for u masss

--- heart.py
+++ heart.py
@@ -2,7 +2,7 @@ import math
 from turtle import *
 from PIL import Image
 
-# Define the heart shape
+# Define the heart shape functions
 def hearta(k):
     return 15 * math.sin(k) ** 3
 
@@ -12,8 +12,8 @@ def heartb(k):
 # Set up the Turtle
 speed(0)  # Set the speed of the drawing
 bgcolor("black")  # Set the background color
-color("red")  # Set the color of the heart
-penup()  # Lift the pen to start from the center
+color("red")  # Set the heart color
+penup()  # Lift the pen to move to the starting point
 goto(0, 0)  # Move the turtle to the center of the screen
 pendown()  # Put the pen down to start drawing
 
@@ -23,12 +23,16 @@ for i in range(3000):  # Loop to plot the heart shape
     y = heartb(i / 100) * 20
     goto(x, y)
 
+# Keep the window open for a few seconds to display the heart
+import time
+time.sleep(5)  # Keep the window open for 5 seconds
+
 # Save the drawing as an image using canvas and PIL
 canvas = getcanvas()
-canvas.postscript(file="heart.eps")  # Save as a .eps file (Encapsulated PostScript)
+canvas.postscript(file="heart.eps")  # Save as an .eps file
 
 # Convert .eps to .png using Pillow
 img = Image.open("heart.eps")
 img.save("heart_image.png")  # Save as a PNG image
 
-done()
+done()  # Close the Turtle window after the drawing is finished
